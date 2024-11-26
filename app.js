57014e60171e40503a280b94ca8d8a70f84c3f06
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-26 18:10:12 +0900

    ok

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const start = async () => {
     try{
         const first = await readFilePromise('./content/first.txt')
         const second = await readFilePromise('./content/second.txt')
-
-        
         console.log(first,second);
+
+        writeFilePromise(
+            './content/result-mind-granade.txt',
+            `This is the awesome : ${first} : ${second}`,
+            {flag: 'a'}
+        )
         
 
     } catch(error) {
