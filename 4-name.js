d0d164c2f811bd2c33e06c693fbb4c043801b2ff
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-26 12:37:24 +0900

    first

---
+++ 4-name.js
@@ -0,0 +1,7 @@
+const sercret = 'super sercret'
+
+const name1 = 'john'
+const name2 = 'smith'
+
+
+module.exports = {name1,name2}
---
+++ app.js
@@ -0,0 +1,11 @@
+const { name1, name2 } = require("./4-name");
+const { sayHi } = require("./5-utils");
+
+
+
+
+sayHi('yukito');
+sayHi(name1);
+sayHi(name2);
+
+
